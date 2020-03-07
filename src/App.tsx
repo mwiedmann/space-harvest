fc056a848f55199f79e5fb0b2b1034a385bb3966
Mark Wiedmann <mwiedmann@inmotionnow.com>
2020-03-06 21:43:46 -0500

    Webpack builds

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { startGame } from './game/game-init'
-
-const App: React.FC = () => {
-  React.useEffect(() => startGame(), [])
-  return null
-}
-
-export default App
---
+++ webpack.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const config = {
+  entry: './src/main.ts',
+  output: {
+    filename: `[name].[hash].min.js`,
+    chunkFilename: `[name].[chunkhash].min.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    // Need to add the .tsx, .ts extensions for ts-loader
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: 'src/images', to: 'images' }]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: false
+    })
+  ]
+}
+
+module.exports = config
